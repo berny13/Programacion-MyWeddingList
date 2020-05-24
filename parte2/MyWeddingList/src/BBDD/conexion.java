d4221fa39efe40da93759c642f80b04c03ebe9d4
berny13 <57494751+berny13@users.noreply.github.com>
2020-05-24 13:10:04 +0200

    se comenta codigo de clase

--- parte2/MyWeddingList/src/BBDD/conexion.java
+++ parte2/MyWeddingList/src/BBDD/conexion.java
@@ -5,12 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+// en esta clase se hace la conexion con la base de datos, se añade clave y usuario para hacerla posible
 public class conexion {
-    
+    //nombre de la base de datos
     private final String base = "listadeinvitados";
+	//nombre de usuario que hara la conexion
     private final String user = "root";
+	//clave que hara la conexion
     private final String password = "manolo1";
+	//numero de puerto al que ira la bbdd
     private final String url = "jdbc:mysql://localhost:3306/" + base;
     private Connection con = null;
     
